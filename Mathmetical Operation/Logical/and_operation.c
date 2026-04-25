408e46fccc5d54cdebca5992928c8654de40fa63
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-04-25 15:50:58 +0600

    logical operation

--- Mathmetical Operation/Logical/and_operation.c
+++ Mathmetical Operation/Logical/and_operation.c
@@ -3,20 +3,20 @@
 
 int main(void)
 {
-    /* Fixed inputs: change these two values as needed. */
+    /* Fixed decimal inputs: change these values as needed. */
     uint32_t a = 23;
     uint32_t b = 45;
     uint32_t result;
 
     __asm volatile(
-        "ORR %0, %1, %2\n"
+        "AND %0, %1, %2\n"
         : "=r"(result)
         : "r"(a), "r"(b)
         : "cc");
 
     printf("A      = %lu\n", (unsigned long)a);
     printf("B      = %lu\n", (unsigned long)b);
-    printf("A | B  = %lu\n", (unsigned long)result);
+    printf("A & B  = %lu\n", (unsigned long)result);
 
     return 0;
 }
